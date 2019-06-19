5038dc95c43c9cb130cc1b27ab5a092f81b2f655
raukuma1988 <raushan10dm087@gmail.com>
2019-06-19 18:57:27 +0530

    changes for cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
+import { CartService } from './../cart.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -8,8 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-
+ product;
+ constructor(
+  private route: ActivatedRoute,
+  private cartService: CartService
+) { }
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
   }
-
+// This method will be used for adding item to the cart
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 }
